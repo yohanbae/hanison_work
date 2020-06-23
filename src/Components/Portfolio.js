6393e4d90831f012367112189eb9c6ea58a42de7
Yohan <gentlebae@gmail.com>
2020-06-23 09:10:16 -0400

    port updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -32,7 +32,7 @@ const Portfolio = () => {
     const smilelandData = {
         title: "SMILE LAND",
         titleImage: "smilelandtitle.png",
-        description: "This is a web-application is new version of Gentle Week. This helps you manage your weekly schedule in the most effective way. Easy to manage your weekly task and easy to see daily achievements. You will be able to enhance your lifestyle through this application.",
+        description: "Did you know there are about 270 muscles on your face move when you smile? We have to smile every day to be healthy. Like many people these days work alone, spend time alone, easy to get their face dry. I created this app to encourage people to smile often. This is a third version of Smileland app which was originally developed as a mobile app. I built this version due to the enhance the accessibility of users. Users don't need to download and install an app, the only thing they need is an internet browser.",
         languages: ["HTML","CSS","REACT" ,"JS","FIREBASE","ILLUSTRATOR"],
         links:[
             {name: "VISIT WEBSITE", link: "https://sweb.netlify.app/"},
@@ -43,10 +43,10 @@ const Portfolio = () => {
     const smilelandMobileData = {
         title: "SMILE LAND MOBILE",
         titleImage: "smilelandmobile1.png",
-        description: "This is a web-application is new version of Gentle Week. This helps you manage your weekly schedule in the most effective way. Easy to manage your weekly task and easy to see daily achievements. You will be able to enhance your lifestyle through this application.",
-        languages: ["HTML","CSS","REACT NATIVE with EXPO","FIREBASE"],
+        description: "Smileland was originally built for mobile. This is a smile tracker app that encourages users to smile often in daily life. Basically the camera on the phone will detect the user's face and will give life to the animals in smileland if the user is smiling forward to the camera. The app was developed with React Native with Expo.",
+        languages: ["HTML","CSS","REACT NATIVE with EXPO"],
         links:[
-            {name: "DOWNLOAD APK", link: "https://github.com/yohanbae/smileland_mobile"},
+            {name: "DOWNLOAD APK", link: "https://drive.google.com/file/d/1vpdzTy1fL03G0GyYYIX0If_qoUV8jDg7/view?usp=sharing"},
             {name: "GITHUB", link: "https://github.com/yohanbae/smileland_mobile"},
         ]
     };
@@ -54,7 +54,7 @@ const Portfolio = () => {
     const sodasuData = {
         title: "SODASU",
         titleImage: "sodasutitle.png",
-        description: "This is a web-application is new version of Gentle Week. This helps you manage your weekly schedule in the most effective way. Easy to manage your weekly task and easy to see daily achievements. You will be able to enhance your lifestyle through this application.",
+        description: "Sodasu is an online survey platform. It has a special feature which is the app remembers the user's past answers. Also, it has a function to find out which answer is major or minor and it compares with the user's answers. So through the answer to the question, the users will find out if their thought is belonging to major or minor. Eventually as many as user answer to the questions, they will see their personal character is belonging to the major or minor. Any users can post any questions. Currently only Korean is served but about to launch the English version soon.",
         languages: ["HTML","CSS","REACT JS","FIREBASE"],
         links:[
             {name: "VISIT WEBSITE", link: "https://sodasu.netlify.app/"},
@@ -63,14 +63,14 @@ const Portfolio = () => {
     };    
 
     const promiseData = {
-        title: "SODASU",
+        title: "PROMISE",
         titleImage: "promisetitle.png",
-        description: "This is a web-application is new version of Gentle Week. This helps you manage your weekly schedule in the most effective way. Easy to manage your weekly task and easy to see daily achievements. You will be able to enhance your lifestyle through this application.",
+        description: "This is a personal schedule app. You can add ToDo list any day you want. You can choose weekly view or monthly view. The main point of this app is beginning from the fact that most people have a weekly pattern in life. Most of the time they repeat the same things weekly. In the app, the user can set up repeating weekly tasks as a template. Once the user sets a template it will be applied to their future schedule automatically so the user doesn't have to input the same tasks over and over. Also, it has a function to calculate task achievement so the user can find out where are they and give the motivation to react to their weekly goal.",
         languages: ["HTML","CSS","REACT JS","FIREBASE"],
         links:[
             {name: "VISIT WEBSITE", link: "https://promisehani.netlify.app/"},
             {name: "GITHUB", link: "https://github.com/yohanbae/reactpromise"},
-            {name: "DOWNLOAD APK", link: "https://github.com/yohanbae/reactpromise"},
+            {name: "DOWNLOAD APK", link: "https://drive.google.com/file/d/11YF6_hdLJDXNUNu7-BSkppr9tN7Yxd56/view?usp=sharing"},
             {name: "MOBILE GITHUB", link: "https://github.com/yohanbae/promise_mobile"}            
         ]
     };        
@@ -78,7 +78,7 @@ const Portfolio = () => {
     const sonmoaData = {
         title: "SONMOA",
         titleImage: "sonmoa.png",
-        description: "This is a web-application is new version of Gentle Week. This helps you manage your weekly schedule in the most effective way. Easy to manage your weekly task and easy to see daily achievements. You will be able to enhance your lifestyle through this application.",
+        description: "Sonmoa is a daily meditation app. Users can set up a timer for their meditation and encourage them to reach daily meditation goals. Users can write a note anytime if they get some inspiration during meditation. Also, it has features of social networks. User can make a friend and share their inspiration. Users can create groups and invite friend so their inspiration notes shared with everyone in the groups.",
         languages: ["HTML","CSS","IONIC","FIREBASE"],
         links:[
             {name: "GITHUB", link: "https://github.com/yohanbae/sonmoa"},
